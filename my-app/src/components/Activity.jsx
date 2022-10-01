31e5bea877005c7185a1331ab29b14a1a95e042c
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2022-10-02 00:09:09 +0200

    adjust add button

--- my-app/src/components/Activity.jsx
+++ my-app/src/components/Activity.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { FlatList, View, Text, StyleSheet } from "react-native";
-import { activitiesArr } from "./HomePage";
 import MyButton from "./MyButton";
+const activitiesArr = [
+  { id: "1", activity: "Go For a Walk." },
+  { id: "2", activity: "Draw." },
+  { id: "3", activity: "Play Free Online Games." },
+  { id: "4", activity: "Take an Online Course." },
+  { id: "5", activity: "Write a short story." },
+  { id: "6", activity: "Learn a new language." },
+  { id: "7", activity: "Go For a Run, Go For a Walk." },
+];
 export default function Activity() {
   const [activities, setActivities] = React.useState(activitiesArr);
   const renderItem = ({ item }) => {
     //  console.log(item);
     const onEditButton = () => {};
     const onDeleteButton = () => {};
-
     return (
       <View style={styles.activityStyle}>
         <Text style={styles.textStyle}>{item.activity}</Text>
@@ -27,8 +34,19 @@ export default function Activity() {
       </View>
     );
   };
+  const onAddButton = () => {
+    var updatedActivities = [...activitiesArr, { id: "8", activity: "walk" }];
+    setActivities(updatedActivities);
+    console.log("Hello!");
+    alert("Activity has been added successfully.");
+  };
   return (
     <View>
+      <MyButton
+        buttonTitle="Add"
+        actionOnPress={onAddButton}
+        buttonColor="#D2B48C"
+      />
       <FlatList
         data={activities}
         renderItem={renderItem}
