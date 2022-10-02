f8bac60d0c5d9b1b97183070edcb724d05a43351
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2022-10-02 16:48:16 +0200

    Add date to each activity

--- my-app/src/components/Activity.jsx
+++ my-app/src/components/Activity.jsx
@@ -3,32 +3,45 @@ import { FlatList, View, Text, StyleSheet, Alert } from "react-native";
 import MyButton from "./MyButton";
 import Header from "./Header";
 const activitiesArr = [
-  { id: 1, activity: "Go For a Walk." },
-  { id: 2, activity: "Draw." },
-  { id: 3, activity: "Play Free Online Games." },
-  { id: 4, activity: "Take an Online Course." },
-  { id: 5, activity: "Write a short story." },
-  { id: 6, activity: "Learn a new language." },
-  { id: 7, activity: "Go For a Run, Go For a Walk." },
+  { id: 1, activity: "Go For a Walk.", date: "29/9/2022 2:50:30" },
+  { id: 2, activity: "Draw.", date: "29/9/2022 3:51:3" },
+  { id: 3, activity: "Play Free Online Games.", date: "1/10/2022 1:51:13" },
+  { id: 4, activity: "Take an Online Course.", date: "1/10/2022  2:52:3" },
+  { id: 5, activity: "Write a short story.", date: "1/10/2022 4:53:23" },
+  { id: 6, activity: "Learn a new language.", date: "1/10/2022 5:55:28" },
+  { id: 7, activity: "Go For a Run, Go For a Walk.", date: "1/10/2022 6:50:30" },
 ];
 export default function Activity() {
   const [inputActivity, setInputActivity] = React.useState("");
   const [activities, setActivities] = React.useState(activitiesArr);
   const [numOfID, setID] = React.useState(8);
+  const [date, setDateTime] = React.useState("");
   const [textLabel, setTextLabel] = React.useState("Enter your activity");
   const enteredActivity = (input) => {
     setInputActivity(input);
     console.log(input);
   };
+  const getDate = () => {
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth() + 1;
+    var year = date.getFullYear();
+    var hours = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    return day + "/" + month + "/" + year + " " + hours + ":" + min + ":" + sec;
+  };
   const onAddButton = () => {
+    const curDate = getDate();
     if (inputActivity.length != 0) {
       const updatedActivities = [
         ...activities,
-        { id: numOfID, activity: inputActivity },
+        { id: numOfID, activity: inputActivity, date: curDate },
       ];
       setID(numOfID + 1);
       setActivities(updatedActivities);
 
+      setDateTime(curDate);
       console.log("Activity has been added.");
       alert("Activity has been added successfully.");
     } else {
@@ -70,12 +83,16 @@ export default function Activity() {
     setActivities(afterDelete);
     console.log(activities);
   };
+
   const renderItem = ({ item }) => {
     //  console.log(item);
 
     return (
       <View style={styles.activityStyle}>
-        <Text style={styles.textStyle}>{item.activity}</Text>
+        <View style={styles.activityContent}>
+          <Text style={styles.textStyle}>{item.activity}</Text>
+          <Text style={styles.dateStyle}>{item.date}</Text>
+        </View>
         <View style={styles.buttonsStyle}>
           <MyButton
             buttonTitle="Edit"
@@ -125,10 +142,19 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
   },
+  activityContent: {
+    alignSelf: "center",
+    borderWidth: 2,
+    borderColor: "red",
+    width: "35%",
+  },
+  dateStyle:{
+    fontWeight: "400",
+    color: "gray"
+  },
   textStyle: {
     fontWeight: "bold",
     fontSize: 18,
-    alignSelf: "center",
   },
   buttonsStyle: {
     flexDirection: "row",
